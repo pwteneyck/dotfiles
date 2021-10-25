e6f65c38ed46ee91c01c6c840d38a9a882ef609a
pwt <pwteneyck@gmail.com>
2021-10-25 14:39:02 -0500

    fixes apt install

--- apt_install.sh
+++ apt_install.sh
@@ -8,12 +8,15 @@ echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sou
 apt update
 apt install sublime-text
 
-cat apt_installs.list | xargs apt install
+cat apt_installs.list | xargs apt -y install
 
 git config --global credential.helper store
+git config --global core.editor "vim"
 
 # install 1Password CLI
 mkdir op_stuff
-wget https://cache.agilebits.com/dist/1P/op/pkg/v1.12.2/op_linux_amd64_v1.12.2.zip op_stuff
-mv op_stuff /usr/local/bin
+wget https://cache.agilebits.com/dist/1P/op/pkg/v1.12.2/op_linux_amd64_v1.12.2.zip -P op_stuff
+unzip -d op_stuff op_stuff/op_linux_amd64_v1.12.2.zip
+mv op_stuff/op /usr/local/bin
+rm -rf op_stuff
 echo "Run 'op signin my.1password.com teneyck.shared@gmail.com' to set up 1PasswordCLI"
